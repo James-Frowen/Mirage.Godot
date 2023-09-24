89229b408f6a34385c66fa383233914f5a6958f3
James Frowen <jamesfrowendev@gmail.com>
2023-09-24 14:56:27 +0100

    fixing server not applying remote positions

--- src/Mirage.Godot.Example1/Scripts/NetworkTransform3D.cs
+++ src/Mirage.Godot.Example1/Scripts/NetworkTransform3D.cs
@@ -1,10 +1,13 @@
 using Godot;
 using Mirage;
+using Mirage.Logging;
 
 namespace Example1
 {
     public partial class NetworkTransform3D : NetworkBehaviour
     {
+        private static readonly ILogger logger = LogFactory.GetLogger<NetworkTransform3D>();
+
         [Export] private Node3D _target;
         private Vector3 _previousPos;
         private Quaternion _previousRot;
@@ -16,15 +19,16 @@ namespace Example1
             if (!Identity.IsSpawned)
                 return;
 
-            if (this.IsServer() || this.HasAuthority())
+            if ((this.IsServer() && Identity.Owner == null) || this.HasAuthority())
             {
+                if (logger.LogEnabled()) logger.Log($"CheckChanged: {Identity.NetId}");
                 CheckChanged();
             }
             else
             {
+                if (logger.LogEnabled()) logger.Log($"MoveTowards: {Identity.NetId}");
                 MoveTowards();
             }
-
         }
 
         private void CheckChanged()
@@ -52,17 +56,20 @@ namespace Example1
         [ServerRpc]
         private void SendUpdateRelayed(Vector3 pos, Quaternion rot)
         {
+            if (logger.LogEnabled()) logger.Log($"RPC ToServer: {Identity.NetId}, {pos} {rot}");
             SendUpdate(pos, rot);
         }
         [ClientRpc]
         private void SendUpdate(Vector3 pos, Quaternion rot)
         {
+            if (logger.LogEnabled()) logger.Log($"RPC ToClient: {Identity.NetId}, {pos} {rot}");
             _targetPos = pos;
             _targetRot = rot;
         }
 
         private void MoveTowards()
         {
+            if (logger.LogEnabled()) logger.Log($"MoveTowards: {Identity.NetId}, from[{_target.Position},{_target.Quaternion}] to[{_targetPos},{_targetRot}]");
             _target.Position = _targetPos;
             _target.Quaternion = _targetRot;
         }
